5f19105bacd4ff8ba604e9fdd8bd1b454fdbf10d
Sam <sam123@gmail.com>
2025-05-16 10:51:54 +0000

    docker-run

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-        res.send('Hello and Welcome from Nodejs App from Deploy project using docker');
+        res.send('Hello and Welcome from Nodejs App from Deploy project using docker and container');
 });
 
 const PORT = process.env.PORT || 3000;
